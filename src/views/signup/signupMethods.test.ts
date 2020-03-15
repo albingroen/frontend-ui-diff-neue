b3d68ed2b002d9372c6bec34b9376b88b82dd8ff
Albin Groen <albin.groen@gmail.com>
2020-03-15 14:56:20 +0100

    🔥 Remove useless test

--- src/views/signup/signupMethods.test.ts
+++
@@ -1,8 +0,0 @@
-import { signupMethods } from "./signupMethods";
-
-test("gets proper signup method", () => {
-  expect(signupMethods[0].name).toStrictEqual('GitHub');
-  expect(signupMethods[1].name).toStrictEqual('GitLab');
-  expect(signupMethods[2].name).toStrictEqual('Google');
-  expect(signupMethods[3].name).toStrictEqual('Email & password');
-});
