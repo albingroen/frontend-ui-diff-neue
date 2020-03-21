173512c380603da825713b4fec2eb68fb7a5941d
Albin Groen <albin.groen@gmail.com>
2020-03-21 13:31:17 +0100

    🎉 Start on error handling

--- src/views/signup/index.tsx
+++ src/views/signup/index.tsx
@@ -6,6 +6,7 @@ import Right from "./components/right";
 import { UserContext } from "../../context/userContext";
 import auth from "../../lib/auth";
 import Loading from "../../components/loading";
+import Error from "../../components/error";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -14,16 +15,25 @@ const Wrapper = styled.div`
 
 export const Signup: React.FC = () => {
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [error, setError] = React.useState<Error | null>();
   const { setUser } = React.useContext(UserContext);
 
   React.useEffect(() => {
     const { code, method } = queryString.parse(window.location.search);
 
     if (code && typeof code === "string" && typeof method === "string") {
-      setIsLoading(true);
-      auth.social.signup(method, code).catch(() => {
-        setIsLoading(false)
-      })
+      (async () => {
+        setIsLoading(true);
+        const res = await auth.social.signup(method, code);
+
+        if (res) {
+          setIsLoading(false);
+          setError(res);
+          setTimeout(() => {
+            setError(null);
+          }, 5000);
+        }
+      })();
     }
   }, [setUser]);
 
@@ -33,6 +43,7 @@ export const Signup: React.FC = () => {
     <Wrapper>
       <Side />
       <Right />
+      {error && <Error error={error.message} />}
     </Wrapper>
   );
 };
