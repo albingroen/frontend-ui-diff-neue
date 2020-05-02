72ba040cf0cf3996fbf48e30b4dd3f2a042bea2b
Albin Groen <albin.groen@gmail.com>
2020-05-02 21:03:52 +0200

    🔥 Add seo title for every page

--- src/views/new-project/index.tsx
+++ src/views/new-project/index.tsx
@@ -1,10 +1,23 @@
 import * as React from 'react'
+import { Helmet } from 'react-helmet'
 import Header from './components/header'
 import Form from './components/form'
+import { useIntl } from 'react-intl'
 
 export const NewProject = () => {
+  const intl = useIntl()
+
   return (
     <div>
+      <Helmet>
+        <title>
+          {intl.formatMessage({
+            defaultMessage: 'New project | ui-diff',
+            id: 'new-project.seo.title'
+          })}
+        </title>
+      </Helmet>
+
       <Header />
       <Form />
     </div>
