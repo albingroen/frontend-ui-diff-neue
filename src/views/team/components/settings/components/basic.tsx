2245aecf1b9a1acf43f6978deca937d079694d45
Albin Groen <albin.groen@gmail.com>
2020-05-09 00:04:17 +0200

    🔥 Improve ux on avatar input

--- src/views/team/components/settings/components/basic.tsx
+++ src/views/team/components/settings/components/basic.tsx
@@ -65,7 +65,7 @@ const Basic: React.FC<IBasicProps> = ({ team }) => {
   const isPristine =
     formState.values.name === team?.name && !formState.values.logo
   const isDirty = Object.values(formState.validity).some(
-    (value?: boolean) => !value
+    (value?: boolean) => value === false
   )
 
   // Runs when submitting form
@@ -75,13 +75,21 @@ const Basic: React.FC<IBasicProps> = ({ team }) => {
     if (!isPristine && !isDirty) {
       dispatch({ type: 'SUBMIT' })
 
-      const formData = new FormData()
-      Object.keys(formState.values).forEach((key) => {
-        formData.append(key, formState.values[key])
-      })
+      let patchedTeam
 
-      const patchedTeam = await patchTeam(team?._id, formData)
+      if (formState.values.logo) {
+        const formData = new FormData()
+        Object.keys(formState.values).forEach((key) => {
+          formData.append(key, formState.values[key])
+        })
+
+        patchedTeam = await patchTeam(team?._id, formData)
+      } else {
+        patchedTeam = await patchTeam(team?._id, formState.values)
+      }
+
       if (patchedTeam && patchedTeam._id) {
+        formState.resetField('logo')
         dispatch({ type: 'SUCCESS' })
         setTimeout(() => {
           dispatch({ type: 'RESET' })
@@ -138,7 +146,11 @@ const Basic: React.FC<IBasicProps> = ({ team }) => {
 
           <Box mt={4}>
             <AvatarInput
-              value={team.logo}
+              value={
+                formState.values.logo
+                  ? URL.createObjectURL(formState.values.logo)
+                  : team.logo
+              }
               label={intl.formatMessage(messages.labelLogo)}
               description={intl.formatMessage(messages.descriptionLogo)}
               onChange={(e) => formState.setField('logo', e.target.files[0])}
