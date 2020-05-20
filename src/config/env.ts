dc9000bd03a56f2d8c0bed7077a4c8bc0b08e60c
albingroen <albin.groen@gmail.com>
2020-05-20 17:23:09 +0200

    🔥 Use prod api if on prod frontend

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,6 +1,7 @@
 export const getEnv = () => {
   const apiEnvs: { [frontendEnv: string]: string } = {
-    localhost: 'http://localhost:5000'
+    localhost: 'http://localhost:5000',
+    'app.ui-diff.com': 'https://ui-diff-api.herokuapp.com'
   }
 
   return apiEnvs[window.location.hostname]
